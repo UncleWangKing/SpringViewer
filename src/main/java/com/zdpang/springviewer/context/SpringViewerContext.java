6209729c0c8b0c76997800163b3b06900f2c3af2
UncleWangKing <285296372@qq.com>
2019-02-05 17:01:26 +0800

    解析带有相应注解的类

--- src/main/java/com/zdpang/springviewer/context/SpringViewerContext.java
+++ src/main/java/com/zdpang/springviewer/context/SpringViewerContext.java
@@ -6,6 +6,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -17,9 +18,17 @@ public class SpringViewerContext implements ApplicationContextAware{
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        /**
+         * 根据类注解删选类对象
+         */
         for (String name:beanDefinitionNames) {
             Object bean = applicationContext.getBean(name);
-            controllerClassBeans.add(ClassBeanParser.getByanno(bean.getClass()));
+            if(null != bean) {
+                ClassBean byClassAnno = ClassBeanParser.getByClassAnno(bean.getClass(), RestController.class);
+                if (null != byClassAnno) {
+                    controllerClassBeans.add(byClassAnno);
+                }
+            }
         }
     }
 
--- src/main/java/com/zdpang/springviewer/parser/ClassBeanParser.java
+++ src/main/java/com/zdpang/springviewer/parser/ClassBeanParser.java
@@ -3,9 +3,12 @@ package com.zdpang.springviewer.parser;
 import com.zdpang.springviewer.bean.ClassBean;
 
 public class ClassBeanParser {
-    public static ClassBean getByanno(Class clazz){
-        ClassBean classBean = new ClassBean();
-        classBean.setClazz(clazz);
-        return classBean;
+    public static ClassBean getByClassAnno(Class clazz, Class clazzAnno){
+        if (clazz.isAnnotationPresent(clazzAnno)) {
+            ClassBean classBean = new ClassBean();
+            classBean.setClazz(clazz);
+            return classBean;
+        }
+        return null;
     }
 }
